2aef9811ec3a0906a8712d9c3ebc0eaa3efedfa1
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-07-17 16:17:59 -0700

    Clean Up.

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -21,3 +21,14 @@ class TestAmenity(unittest.TestCase):
     def test_class_attr(self):
         self.assertIs(type(self.amenity.name), str)
         self.assertFalse(bool(getattr(self.amenity, "name")))
+
+    def __init__(self, *args, **kwargs):
+        """ """
+        super().__init__(*args, **kwargs)
+        self.name = "Amenity"
+        self.value = Amenity
+
+    def test_name2(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.name), str)
